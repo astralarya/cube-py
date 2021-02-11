e17f0cb3599ac29b4a6a9f55a451a13fa411b71f
Mara Kim <mara@autochthe.net>
2021-02-10 22:27:05 -0500

    Outfile creation

--- mtg_cube/__main__.py
+++ mtg_cube/__main__.py
@@ -1,14 +1,19 @@
 import click
-import sys
+from pathlib import Path
 
 from mtg_cube.io import mtgo_csv
 
 
 @click.command()
 @click.argument("input", type=click.File("r"))
-def main(input):
+@click.option("--output", "-o", type=click.Path(), default="out.csv")
+def main(input, output):
     cards = mtgo_csv.read(input)
-    mtgo_csv.write(sys.stdout, cards)
+    pools = [cards]
+    outpath = Path(output)
+    for idx, pool in enumerate(pools):
+        with outpath.with_suffix(f".{idx}{outpath.suffix}").open("w") as outfile:
+            mtgo_csv.write(outfile, pool)
 
 
 if __name__ == "__main__":
